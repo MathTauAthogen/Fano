4c1e279f6cebc5533ad8ded407f8b3a0cc309709
William Qin <wqin2008@gmail.com>
2022-12-10 02:47:37 +0000

    Stuff

--- README.md
+++ README.md
@@ -5,3 +5,19 @@
 Credit must go where credit is due, and this game is stolen straight off of Pagat's Invented Games.
 
 I've implemented this first as a command line game, but obviously it is much better to turn it into a game beyond this.
+
+## Notes to self
+
+### Commands
+
+Whenever I change the Javascript, go into the js folder and run the following command:
+
+`browserify main.js -o bundle.js`
+
+to sync it all up.
+
+Also,
+
+`python3 -m http.server`
+
+in the base Fano directory is required to bypass Firefox's CORS request stuff.
--- js/main.js
+++ js/main.js
@@ -1,5 +1,25 @@
-console.log("Here!");
+var gamejs = require('gamejs');
 
-const firstcard = document.createElement("img");
-firstcard.src = "../assets/bluedown.png";
-document.body.appendChild(firstcard);
+// Preload assets
+
+suits = ["spade","club","heart","diamond"]
+
+for (let i = 2; i < 11; i++){
+	for (let j = 0; j < 4; j++){
+		gamejs.preload([suits[j] + i + ".png"])
+	}
+}
+
+royals = ["ace", "jack", "queen", "king"]
+
+for (let i = 0; i < 4; i++){
+	for (let j = 0; j < 4; j++){
+		gamejs.preload([suits[j] + royals[i] + ".png"])
+	}
+}
+
+for (i in ["down", "joker"]){
+	for (j in ["blue", "red"]){
+		gamejs.preload([j + i + ".png"])
+	}
+}
